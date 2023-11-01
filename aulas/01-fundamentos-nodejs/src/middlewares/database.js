64b45a3aa7533088debdf255593b18da17139c00
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-01 16:24:20 -0300

    finalizando busca por query params

--- aulas/01-fundamentos-nodejs/src/middlewares/database.js
+++ aulas/01-fundamentos-nodejs/src/middlewares/database.js
@@ -8,7 +8,6 @@ export class Database {
     fs.readFile(databasePath, "utf-8")
       .then((data) => {
         this.#database = JSON.parse(data);
-        
       })
       .catch(() => {
         this.#persist();
@@ -18,14 +17,23 @@ export class Database {
   #persist() {
     fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
-  select(table) {
-    const data = this.#database[table] ?? [];
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+
     return data;
   }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data)
+      this.#database[table].push(data);
     } else {
       this.#database[table] = [data];
     }
@@ -33,20 +41,20 @@ export class Database {
 
     return data;
   }
-  delete(table,id){
-    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
-    if(rowIndex > -1) {
-      this.#database[table].splice(rowIndex,1)
-      this.#persist()
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
     }
   }
-  update(table,id, data){
-    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
-    if(rowIndex > -1) {
-      this.#database[table][rowIndex] = {id,...data}
-      this.#persist()
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data };
+      this.#persist();
     }
   }
 }
