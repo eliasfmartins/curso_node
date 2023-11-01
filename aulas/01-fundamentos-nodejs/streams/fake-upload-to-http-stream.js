36e7edf4990e3695b80548ec66754fcb356fa9f8
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-01 11:09:44 -0300

    usando isominia  e setando dados no corpoo da requisicao

--- aulas/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
+++ aulas/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -1,22 +1,27 @@
-import {Readable } from 'node:stream'
+import { Readable } from "node:stream";
 
 class OneToHundredStream extends Readable {
-  index = 1
-  _read(){
-    const i = this.index++
-    setTimeout(()=>{
-      if (i>100) {
-        this.push(null)
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 5) {
+        this.push(null);
       } else {
-        const buf = Buffer.from(String(i))
-        this.push(buf)
+        const buf = Buffer.from(String(i));
+        this.push(buf);
       }
-    },1000)
-    }
-    
+    }, 1000);
+  }
 }
 
-fetch('http://localhost:3334',{
-  method: 'POST',
-  body: new OneToHundredStream() ,
-})
+fetch("http://localhost:3334", {
+  method: "POST",
+  body: new OneToHundredStream(),
+}).then((response) => {
+  response.text().then((data) => {
+    console.log(data);
+  });
+});
