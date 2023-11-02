e98b0b0bfb2a2e842132a99ff5492521796a5108
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-02 16:50:05 -0300

    add knex e banco da dados

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
@@ -6,9 +7,15 @@ const app = fastify()
 // GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  // const transactions = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transacao de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+  const transactions = await knex('transactions').where('amount', 1000)
+  return transactions
 })
 
 app
