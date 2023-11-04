9999ff20a0b3eeca00973899d834e80cd0395cee
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-03 21:29:16 -0300

    plugins e rotas

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,24 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactiions'
 
 const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
-
-app.get('/hello', async () => {
-  // const transactions = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transacao de teste',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
-  const transactions = await knex('transactions').where('amount', 1000)
-  return transactions
-})
-
+app.register(transactionsRoutes)
 app
   .listen({
     port: env.PORT,
