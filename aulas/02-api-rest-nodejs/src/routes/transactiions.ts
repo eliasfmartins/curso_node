d63284c59786720405fb45c4cc5a6c6959f84e98
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-04 14:18:24 -0300

    typando knex

--- aulas/02-api-rest-nodejs/src/routes/transactiions.ts
+++ aulas/02-api-rest-nodejs/src/routes/transactiions.ts
@@ -1,9 +1,67 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions').where('amount', 1000)
-    return transactions
+  app.get('/', async (request, reply) => {
+    const sessionId = request.cookies.sessionId
+    if (!sessionId) {
+      return reply.status(401).send({
+        error: 'Unauthorized.',
+      })
+    }
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
+
+    return { transactions }
+  })
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    const transactions = await knex('transactions').where('id', id).first()
+
+    return { transactions }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionsBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+    const { type, amount, title } = createTransactionsBodySchema.parse(
+      request.body,
+    )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
+    return reply.status(201).send()
   })
 }
--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,11 +1,15 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactiions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
-app.register(transactionsRoutes)
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 app
   .listen({
     port: env.PORT,
