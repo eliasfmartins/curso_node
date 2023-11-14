b243c20cbdf0fd25e4f8b31728972cd2f5320583
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-14 18:26:52 -0300

    create route post users

--- aulas/03-api-solid/src/app.ts
+++ aulas/03-api-solid/src/app.ts
@@ -1,14 +1,25 @@
 import fastify from "fastify";
 
-import {PrismaClient} from "@prisma/client";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-prisma.user.create({
-	data:{
-		name: "Elias F Martins",
-		email: "eliasmartins@gmail.com",
-    
-	}
+app.post("/users" , async (req, reply) =>{
+	const registerbodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6)
+	});
+
+	const { name, email, password} = registerbodySchema.parse(req.body);
+	await prisma.user.create({
+		data:{
+			name,
+			email,
+			password_hash: password,
+		}
+	});
+
+	return reply.status(201).send();
 });
